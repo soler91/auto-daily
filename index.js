673f16d314cbd2d9f2715cabcfc89d706708a242
Waow-Tuturu <ujeshman@gmail.com>
2017-07-18 11:55:30 -0400

    Separated elite and emporium. Works w/o elite.

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ module.exports = function test(dispatch) {
 const command = Command(dispatch)
 
 let Daily_Boxes = [169660, 180675], // Deliver Elite Gift box , Deliver Elite Consumable Box
-    slots = [];
+    slots = [],
+    EmpLeftToClaim;
 
     dispatch.hook('S_LOAD_CLIENT_USER_SETTING', 1, () => {
         if(slots.length > 0){
         setTimeout(function(){ command.message('Claiming Dailies in ' + Wait_time + ' seconds.'); }, Load_time);
-        setTimeout(claimItems, (Wait_time+Load_time)*1000);
+        setTimeout(claimElite, (Wait_time+Load_time)*1000);
         }
+        if(EmpLeftToClaim) {
+            setTimeout(function(){ command.message('Claiming emporium in ' + Wait_time + ' seconds.'); }, Load_time);
+            setTimeout(claimEmporium, (Wait_time+Load_time)*1000);
+        }
+    });
+    
+    dispatch.hook('S_SEND_VIP_SYSTEM_INFO', 1, event => {
+        EmpLeftToClaim = (event.dailyCredits) ? true : false;
     });
 
 	dispatch.hook('S_PCBANGINVENTORY_DATALIST', 1, event => {
@@ -26,14 +35,17 @@ let Daily_Boxes = [169660, 180675], // Deliver Elite Gift box , Deliver Elite Co
         }
     });
     
-    function claimItems(){
-        dispatch.toServer('C_REQUEST_RECV_DAILY_TOKEN', 1, {});
+    function claimElite(){
         for(let slot of slots){
             dispatch.toServer('C_PCBANGINVENTORY_USE_SLOT', 1, {
                 slot: slot
             });
         }
-        command.message('Claimed Dailies.')
+        command.message('Claimed Elite Boxes.')
         slots = [];
+    }
+    
+    function claimEmporium(){
+        dispatch.toServer('C_REQUEST_RECV_DAILY_TOKEN', 1, {});
     }
 }
